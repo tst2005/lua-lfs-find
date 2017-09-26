417b5afa34c204f67aa2c9243f34f1d039162e37
TsT <tst2005@gmail.com>
2017-09-26 14:19:29 +0200

    mindepth and maxdepth support

--- lfs-find.lua
+++ lfs-find.lua
@@ -1,49 +1,83 @@
 -- See http://lua-users.org/wiki/DirTreeIterator
-local workaround = function(attr)
+local M = {}
+
+-- the lfs API return a table in most case, except if you ask only one mode.
+-- this workaround make we always get a table
+local workaround = function(attr, wantedattr)
 	if type(wantedattr)=="string" and type(attr)=="string" then
 		return {[wantedattr]=attr}
 	end
-	assert(type(attr) == "table")
+	--assert(type(attr) == "table", "attr is not a table")
 	return attr
 end
 
-return function(lfs)
-	local lfs = lfs 
-	local function attrdir(path, opt)
-		local hook_ok, hook_error = opt.hook_ok, hook_error
-		local depth = opt.depth
-		local wantedattr = opt.attr
+function M:_attrdir(path, level)
+	assert(type(path)=="string", "invalid path argument")
+	local lfs = assert(self.lfs)
+	local opt = assert(self.opt)
+	local hook_ok = assert(opt.hook_ok)
+	local depth = opt.depth
+	local wantedattr = opt.attr
+	local misc = {level=level}
+	local mindepth_ok = not opt.mindepth or (level >= opt.mindepth)
+	local maxdepth_ok = not opt.maxdepth or (level < opt.maxdepth)
 
-		-- TODO: pcall(lfs.* ...) in case of error
+	local attr
+	local ok, err = pcall(function()
+		attr = lfs.symlinkattributes(path, opt.attr) -- get attribute (no-follow)
+	end)
+	misc.attr=attr
+	if not ok then
+		opt.hook_error(path, attr and attr.mode or "", attr, misc)
+		return
+	end
+	attr = workaround(attr, opt.attr) -- we want a `attr` table
 
-		local attr
-		local ok, err = pcall(
-			function()
-				attr = lfs.symlinkattributes(path, opt.attr) -- get attribute (no-follow)
-			end
-		)
-		if not ok then
-			hook_error(path, attr and attr.mode or "", attr)
-			return
-		end
-		attr = workaround(attr)
-		
-		if attr.mode == "directory" then
-			if not depth then opt.hook_ok(path, attr.mode, attr) end
+	if attr.mode == "directory" then
+		if not depth and mindepth_ok then opt.hook_ok(path, attr.mode, attr, misc) end
+		--if not opt.maxdepth or (level < opt.maxdepth) then
+		if maxdepth_ok then
 			if not pcall(function()
 				for file in lfs.dir(path) do
 					if file ~= "." and file ~= ".." then
 						local subpath = path..'/'..file
-						attrdir(subpath, opt)
+						self:_attrdir(subpath, level+1)
 					end
 				end
 			end) then
-				opt.hook_error(path, attr and attr.mode or "", attr)
+				opt.hook_error(path, attr and attr.mode or "", attr, misc)
 			end
-			if depth then hook_ok(path, attr.mode, attr) end
-		else
-			hook_ok(path, attr.mode, attr)
+		end
+		if depth and mindepth_ok then hook_ok(path, attr.mode, attr, misc) end
+	else
+		if mindepth_ok then
+			-- is a file or everything except directory
+			hook_ok(path, attr.mode, attr, misc)
 		end
 	end
-	return attrdir
 end
+
+local function tablecopy(src, dst)
+	if dst==nil then dst={} end
+	for k,v in pairs(src) do
+		dst[k] = v
+	end
+	return dst
+end
+
+local function default_hook_error(path, mode, attr, misc)
+end
+local function default_hook_ok(path, mode, attr, misc)
+end
+
+local default_opt = {hook_error=default_hook_error, hook_ok=default_hook_ok, depth=nil, attr=nil, mindepth=nil, maxdepth=nil}
+function M:find(path, opt)
+	if self.lfs==nil then 
+		self.lfs = require "lfs"
+	end
+	self.opt = tablecopy(opt, tablecopy(default_opt))
+	return self:_attrdir(path, 0)
+end
+
+setmetatable(M, {__call = function(_, ...) return M:find(...) end})
+return M
--- testcode.lua
+++ testcode.lua
@@ -1,8 +1,20 @@
 return function(find, ...)
-	
+
 	local arg = {...}
-	
-	local function hook(f, mode, attr)
+--	local xdev = nil
+
+	local function hook(self, f, mode, attr, misc)
+
+		assert(attr.dev, "no attr.dev")
+--		if attr.dev then
+--			if xdev~=nil and xdev~= attr.dev then -- dev changed!
+--				f="#(not-same-dev) "
+--			else
+--				f="ok "
+--			end
+--			xdev=attr.dev
+--		end
+
 		if mode == "directory" then
 			print(f)
 			for name, value in pairs(attr) do
@@ -15,14 +27,14 @@ return function(find, ...)
 			end
 		end
 	end
-	
-	local function hook(f, mode, attr)
-		print(f..(mode=="directory" and "/" or ""))
+
+	local function hook(f, mode, attr, misc)
+		print((misc.level or "").." "..f..(mode=="directory" and "/" or ""))
 	end
-	
-	local function hookerr(f, mode, attr)
+
+	local function hookerr(f, mode, attr, misc)
 		io.stderr:write(""..f..": Permission denied ?\n")
 	end
-	
-	return find(arg[1], {depth=true, attr=nil--[["mode"]], hook_ok = hook, hook_error = hookerr})
+
+	return find(arg[1], {depth=nil, attr=nil--[["mode"]], hook_ok = hook, hook_error = hookerr, maxdepth=2, mindepth=1})
 end
